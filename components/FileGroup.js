ab78993be860c5986435d6a5d8c952316ac09b6a
khayatyassine7-a11y <khayatyassine7@gmail.com>
2026-03-25 17:30:37 +0100

    Add files via upload

--- components/FileGroup.js
+++ components/FileGroup.js
@@ -1,1 +1,25 @@
-
+export default function FileGroup({ title, fieldA, fieldB, files, setFiles }) {
+  return (
+    <div className="card">
+      <h3 style={{ marginTop: 0 }}>{title}</h3>
+      <div className="grid">
+        <div className="filebox">
+          <label className="label">Recto</label>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => setFiles({ ...files, [fieldA]: e.target.files?.[0] || null })}
+          />
+        </div>
+        <div className="filebox">
+          <label className="label">Verso</label>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => setFiles({ ...files, [fieldB]: e.target.files?.[0] || null })}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
