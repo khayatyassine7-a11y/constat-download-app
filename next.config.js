a1b609ecc24bfbd6a59e5edf13a0462300190b51
khayatyassine7-a11y <khayatyassine7@gmail.com>
2026-03-27 00:29:31 +0100

    Add webpack fallback configuration for fs and child_process

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      child_process: false
+    };
+    return config;
+  }
 };
+
 module.exports = nextConfig;
